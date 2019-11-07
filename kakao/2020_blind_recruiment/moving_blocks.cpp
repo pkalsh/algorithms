0632e562b7f3824abb894440b2d82d491927cd89
pkalsh345 <pkalsh345@gmail.com>
2019-11-07 20:49:16 +0900

    solve moving_blocks (compile error)

--- kakao/2020_blind_recruiment/moving_blocks.cpp
+++ kakao/2020_blind_recruiment/moving_blocks.cpp
@@ -13,16 +13,32 @@ point;
 typedef struct RobotPos {
     point first;
     point second;
+    int isVertical;
 }
 robotPos;
 
-bool isHorizontal = true;
+int dr[4] = {0, 1, 0, -1};
+int dc[4] = {1, 0, -1, 0};
+bool isrev[4] = {true, true, false, false};
+bool isfir[4] = {true, false, true, false};
 
-vector<vector<int> >discovered;
+// first 기준
+// [0] 가로 방향 visit 체크
+// [1] 세로 방향 visit 체크 
+vector<vector<int> >discovered[2];
+
+void init(vector<vector<int> >&);
+void makeNewGraph(vector<vector<int> >&);
+bool canRotate(vector<vector<int> >&, robotPos&, bool, bool);
+void rotateRobot(robotPos&, bool, bool);
+bool canMove(vector<vector<int> >&, robotPos&, point);
+bool isDiscovered(robotPos&, point);
+int solution(vector<vector<int> >);
 
 void init(vector<vector<int> >& board) {
 	makeNewGraph(board);
-	discovered = board;	
+	discovered[0] = board;
+    discovered[1] = board;
 }
 void makeNewGraph(vector < vector<int> >& board) {
     size_t old_size = board.size();
@@ -35,13 +51,48 @@ void makeNewGraph(vector < vector<int> >& board) {
     }
     board = temp;
 }
+bool canRotate(vector<vector<int> >& board, robotPos& now, bool rotReverse, bool rotFirst) {
+    int bottom_first = board[now.first.row + 1][now.first.col];
+    int bottom_second = board[now.second.row + 1][now.first.col];
+    int up_first = board[now.first.row - 1][now.first.col];
+    int up_second = board[now.second.row - 1][now.second.col];
+
+    if (rotReverse) {
+        if (rotFirst) {
+            if (!up_first && !up_second ) {
+                return true;
+            }
+            return false;
+        }
+        else {
+            if (!bottom_first && !bottom_second) {
+                return true;
+            }
+            return false;
+        }
+    }
+    else {
+        if (rotFirst) {
+            if (!bottom_first && !bottom_second) {
+                return true;
+            }
+            return false;
+        }
+        else {
+            if (!up_first && !up_second) {
+                return true;
+            }
+            return false;
+        }
+    }
+}
 void rotateRobot(robotPos & now, bool rotReverse, bool rotFirst) {
     // 회전할 수 있는지 없는지 판단은 다른 메소드 몫
 	// rotReverse -> 시계 반대방향, rotFirst -> first를 축으로
     point old_first = now.first;
     point old_second = now.second;
 
-    if (isHorizontal) {
+    if (!now.isVertical) {
         // 가로 방향일때
         if (rotReverse) {
             // 시계 반대 방향으로 돌린다.
@@ -76,6 +127,8 @@ void rotateRobot(robotPos & now, bool rotReverse, bool rotFirst) {
                 };
             }
         }
+        // 세로 방향이 된 first 위치를 visit 처리
+        discovered[now.first.row][now.first.col][1] = 1;
     } else {
         // 세로 방향일때
         if (rotReverse) {
@@ -111,6 +164,49 @@ void rotateRobot(robotPos & now, bool rotReverse, bool rotFirst) {
                 };
             }
         }
+        // 가로 방향이 된 first 위치를 visit 처리
+        discovered[now.first.row][now.first.col][0] = 1;
+    }
+}
+bool canMove(vector<vector<int> >& board, robotPos& now, point direction) {
+    int destF, destS;
+    destF = board[now.first.row + direction.row][now.first.col + direction.col];
+    destS = board[now.second.row + direction.row][now.second.col + direction.col];
+
+    if (now.isVertical) {
+        if (direction.row == 0) {
+            return (!destF && !destS);
+        }
+        else {
+            if (direction.row < 0) {
+                return !destF;
+            }
+            else {
+                return !destS;
+            }
+        }
+    }
+    else {
+        if (direction.row == 0) {
+            if (direction.col < 0) {
+                return !destF;
+            }
+            else {
+                return !destS;
+            }
+        }
+        else {
+            return (!destF && !destS);
+        }
+    }
+}
+bool isDiscovered(robotPos& now, point direction) {
+    if (!now.isVertical) {
+        // 가로 방향일때
+        return discovered[now.first.row + direction.row][now.first.col + direction.col][0];
+    }
+    else {
+        return discovered[now.first.row + direction.row][now.first.col + direction.col][1]; 
     }
 }
 
@@ -118,18 +214,50 @@ int solution(vector<vector<int>> board) {
     int answer = 0;
 	
 	init(board);
+    int dest_pos = board.size() - 2;
 	// BFS
 	queue<robotPos> q;
-	q.push(robotPos{point{1,1},point{1,2}});
-	discovered[1][1] = 1;
+	q.push(robotPos{point{1,1}, point{1,2}, false});
+	discovered[1][1][0] = 1;
+    int be_processed = q.size();
 
 	while (!q.empty()) {
 		robotPos now_pos = q.front();
+        // 도달하면 반복문 탈출
+        if (now_pos.second.row == dest_pos && now_pos.second.col == dest_pos) 
+            break;
+        if (be_processed == 0) {
+            be_processed = q.size();
+            answer++;
+        }
+
 		q.pop();
+        be_processed--;
 		
+        // 이동
+        for (int i = 0; i < sizeof(dr)/sizeof(int); i++) {
+            point dir = {dr[i], dc[i]};
+            if (canMove(board, now_pos, dir) && !isDiscovered(now_pos, dir)) {
+                robotPos new_pos;
+                new_pos.first = {now_pos.first.row + dir.row, now_pos.first.col + dir.col};
+                new_pos.second = {now_pos.second.row + dir.row, now_pos.second.col + dir.col};
+                new_pos.isVertical = now_pos.isVertical;
+
+                q.push(new_pos);
+                discovered[now_pos.first.row + dir.row][now_pos.first.col + dir.col][now_pos.isVertical] = 1;
+            }
+        }
+        // 회전
+        for (int j = 0; j < sizeof(isrev)/sizeof(bool); j++) {
+            bool rot_reverse = isrev[j];
+            bool rot_first = isfir[j];
+            if (canRotate(board, now_pos, rot_reverse, rot_first)) {
+                robotPos new_pos = now_pos;
+                rotateRobot(new_pos, rot_reverse, rot_first);
+                q.push(new_pos);
+            }
+        }
 	}
 	
     return answer;
 }
-
-// isHorizontal이 true이고 점이 <N, N-1>에 도달하거나 false이고 점이 <N-1, N>에 도달하면 성공이다.
